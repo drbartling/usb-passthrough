faaf2f20dc134d04ff5a3c71ca7a8284ceb89539
Ryan Bartling <rbartling@axon.com>
2024-12-24 12:32:29 -0700

    Make the CDC check assert we get back what we sent

--- cdc-check.py
+++ cdc-check.py
@@ -6,12 +6,12 @@ import serial
 ports = glob.glob("/dev/ttyACM*")
 ports.extend(glob.glob("/dev/ttyUSB*"))
 baud_rate = 115200
-test_str = b",".join([f"World{i}".encode("utf-8") for i in range(0, 20)])
+test_str = b",".join([f"World{i}".encode("utf-8") for i in range(0, 100)])
 
 for com_port in ports:
     with serial.Serial(com_port, baud_rate, timeout=0.2) as ser:
         ser.flushInput()
         ser.reset_input_buffer()
         ser.write(test_str)
-        rsp = ser.read(9999)
-        print(f"{com_port}\r\n\t{rsp}")
+        rsp = ser.read(99999)
+        assert test_str == rsp, f"{com_port} failed check"
