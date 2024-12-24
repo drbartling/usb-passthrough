fbbf472219e630e8c64208a59606f265befae198
Ryan Bartling <rbartling@axon.com>
2024-12-24 16:37:04 -0700

    Split into four tasks
    Was able to send more bytes in loopback mode before assertion failed.
    (See cdc-check.py)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 mod board;
 
 #[cfg(feature = "defmt")]
-use defmt::{assert_eq, info, panic, warn};
+use defmt::{error, info, panic};
 #[cfg(not(feature = "defmt"))]
 use panic_halt as _;
 #[cfg(feature = "defmt")]
@@ -18,13 +18,32 @@ use embassy_stm32::mode::Async;
 use embassy_stm32::peripherals;
 use embassy_stm32::usart::{RingBufferedUartRx, UartTx};
 use embassy_stm32::usb;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::pubsub::{PubSubChannel, Publisher, Subscriber, WaitResult};
 use embassy_usb::class::cdc_acm;
 use embassy_usb::driver::EndpointError;
 use embedded_alloc::LlffHeap as Heap;
 use embedded_io_async::Write;
+use heapless::Vec;
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
+
+type ToUsbBuf = Vec<u8, 63>;
+type ToUsbChannel = PubSubChannel<ThreadModeRawMutex, ToUsbBuf, 5, 1, 1>;
+type ToUsbChannelPublisher =
+    Publisher<'static, ThreadModeRawMutex, ToUsbBuf, 5, 1, 1>;
+type ToUsbChannelSubscriber =
+    Subscriber<'static, ThreadModeRawMutex, ToUsbBuf, 5, 1, 1>;
+static TO_USB: ToUsbChannel = PubSubChannel::new();
+
+type ToUartBuf = Vec<u8, 64>;
+type ToUartChannel = PubSubChannel<ThreadModeRawMutex, ToUartBuf, 5, 1, 2>;
+type ToUartChannelPublisher =
+    Publisher<'static, ThreadModeRawMutex, ToUartBuf, 5, 1, 2>;
+type ToUartChannelSubscriber =
+    Subscriber<'static, ThreadModeRawMutex, ToUartBuf, 5, 1, 2>;
+static TO_UART: ToUartChannel = PubSubChannel::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -36,19 +55,26 @@ async fn main(spawner: Spawner) {
         unsafe { HEAP.init(addr_of_mut!(HEAP_MEM) as usize, HEAP_SIZE) }
     }
     let board = Board::new();
-    let mut usb = board.usb;
-    let usb_fut = usb.run();
 
     let uart_tx = board.uart_tx;
-    let usb_cdc_rx = board.usb_cdc_rx;
-    spawner.must_spawn(usb_to_uart(uart_tx, usb_cdc_rx));
+    let to_payload_sub = TO_UART.subscriber().unwrap();
+    spawner.must_spawn(uart_sender(uart_tx, to_payload_sub));
 
     let usb_cdc_tx = board.usb_cdc_tx;
-    let uart_rx = board.uart_rx;
-    spawner.must_spawn(uart_to_usb(usb_cdc_tx, uart_rx));
+    let to_pc_sub = TO_USB.subscriber().unwrap();
+    spawner.must_spawn(usb_sender(usb_cdc_tx, to_pc_sub));
 
+    let usb_cdc_rx = board.usb_cdc_rx;
+    let to_payload_pub = TO_UART.publisher().unwrap();
+    spawner.must_spawn(usb_receiver(usb_cdc_rx, to_payload_pub));
+
+    let uart_rx = board.uart_rx;
+    let to_pc_pub = TO_USB.publisher().unwrap();
+    spawner.must_spawn(uart_receiver(uart_rx, to_pc_pub));
+
+    let mut usb = board.usb;
     loop {
-        usb_fut.await;
+        usb.run().await;
     }
 }
 
@@ -64,79 +90,101 @@ impl From<EndpointError> for Disconnected {
 }
 
 #[embassy_executor::task]
-async fn usb_to_uart(
+async fn uart_sender(
     mut uart_tx: UartTx<'static, Async>,
-    mut usb_cdc_rx: cdc_acm::Receiver<
-        'static,
-        usb::Driver<'static, peripherals::USB>,
-    >,
+    mut to_uart_sub: ToUartChannelSubscriber,
 ) {
-    // Default packet size is 64
-    #[cfg(feature = "defmt")]
-    assert_eq!(64, usb_cdc_rx.max_packet_size());
     loop {
-        usb_cdc_rx.wait_connection().await;
-        #[cfg(feature = "defmt")]
-        info!("CDC RX Connected");
-        let mut buf = [0; 64];
-        loop {
-            if let Ok(n) = usb_cdc_rx.read_packet(&mut buf).await {
-                let data = &buf[..n];
+        let buf = match to_uart_sub.next_message().await {
+            WaitResult::Lagged(n) => {
                 #[cfg(feature = "defmt")]
-                info!("To UART TX {}: {:x}", n, data);
-                #[allow(unused_variables)]
-                if let Err(e) = uart_tx.write_all(data).await {
-                    #[cfg(feature = "defmt")]
-                    warn!("UART TX err: {:?}", e);
-                    break;
-                }
+                error!("Missed {:?} bytes to send to payload", n);
+                None
+            }
+            WaitResult::Message(buf) => Some(buf),
+        };
+        if let Some(buf) = buf {
+            if let Err(e) = uart_tx.write_all(&buf).await {
+                #[cfg(feature = "defmt")]
+                error!("UART TX err: {:?}", e);
             }
         }
-        #[cfg(feature = "defmt")]
-        warn!("CDC RX Disconnected");
     }
 }
 
 #[embassy_executor::task]
-async fn uart_to_usb(
-    mut usb_cdc_tx: cdc_acm::Sender<
+async fn uart_receiver(
+    mut uart_rx: RingBufferedUartRx<'static>,
+    to_usb_pub: ToUsbChannelPublisher,
+) {
+    let mut buf = [0; 63];
+    loop {
+        let result = uart_rx.read(&mut buf).await;
+        match result {
+            Ok(n) => {
+                let data: ToUsbBuf = buf[..n].try_into().unwrap();
+                #[cfg(feature = "defmt")]
+                info!("From Payload {}: {:x}", n, data.as_slice());
+                to_usb_pub.publish(data).await;
+            }
+            Err(e) => {
+                #[cfg(feature = "defmt")]
+                error!("UART RX error {:?}", e);
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn usb_sender(
+    mut cdc_tx: cdc_acm::Sender<
         'static,
         usb::Driver<'static, peripherals::USB>,
     >,
-    mut uart_rx: RingBufferedUartRx<'static>,
+    mut to_usb_sub: ToUsbChannelSubscriber,
 ) {
-    // Default packet size is 64
-    #[cfg(feature = "defmt")]
-    assert_eq!(64, usb_cdc_tx.max_packet_size());
     loop {
-        usb_cdc_tx.wait_connection().await;
-        #[cfg(feature = "defmt")]
-        info!("CDC TX Connected");
-
-        // Send a max of 1 less than USB max packet size
-        // If we send 64, the USB driver expects more to follow
-        let mut buf = [0; 63];
+        cdc_tx.wait_connection().await;
         loop {
-            let n = match uart_rx.read(&mut buf).await {
-                Ok(n) => n,
-                #[allow(unused_variables)]
-                Err(e) => {
+            let buf = match to_usb_sub.next_message().await {
+                WaitResult::Lagged(n) => {
                     #[cfg(feature = "defmt")]
-                    warn!("UART RX err: {:?}", e);
+                    error!("Missed {:?} packets to send to the payload", n);
+                    None
+                }
+                WaitResult::Message(buf) => Some(buf),
+            };
+            if let Some(buf) = buf {
+                if let Err(e) = cdc_tx.write_packet(&buf).await {
+                    #[cfg(feature = "defmt")]
+                    error!("CDC TX err: {:?}", e);
                     break;
                 }
-            };
+            }
+        }
+    }
+}
 
-            let data = &buf[..n];
-            #[cfg(feature = "defmt")]
-            info!("To CDC TX {}: {:x}", n, data);
-            if let Err(e) = usb_cdc_tx.write_packet(data).await {
+#[embassy_executor::task]
+async fn usb_receiver(
+    mut cdc_rx: cdc_acm::Receiver<
+        'static,
+        usb::Driver<'static, peripherals::USB>,
+    >,
+    to_uart_pub: ToUartChannelPublisher,
+) {
+    loop {
+        cdc_rx.wait_connection().await;
+        let mut buf = [0; 64];
+        loop {
+            if let Ok(n) = cdc_rx.read_packet(&mut buf).await {
+                let data: ToUartBuf = buf[..n].try_into().unwrap();
                 #[cfg(feature = "defmt")]
-                warn!("CDC TX err: {:?}", e);
+                info!("From PC {}: {:x}", n, data.as_slice());
+                to_uart_pub.publish(data).await;
+            } else {
                 break;
             }
         }
-        #[cfg(feature = "defmt")]
-        warn!("CDC TX Disconnected");
     }
 }
