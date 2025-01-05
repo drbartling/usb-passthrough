7770eb17debe54c70b835c86c08eb6765b66207b
Ryan Bartling <rbartling@axon.com>
2025-01-05 14:17:04 -0700

    Went back to four tasks, perf improved
    ./cdc_test.py now takes just under 800 ms.
    There's still gaps between the 64 byte chunk that can be up to 3 ms in
    length

--- usb-passthrough/src/main.rs
+++ usb-passthrough/src/main.rs
@@ -30,13 +30,21 @@ type ToUsbChannelSubscriber =
     Subscriber<'static, ThreadModeRawMutex, ToUsbBuf, 4, 1, 1>;
 static TO_USB: ToUsbChannel = PubSubChannel::new();
 
+type ToUartBuf = Vec<u8, 64>;
+type ToUartChannel = PubSubChannel<ThreadModeRawMutex, ToUartBuf, 4, 1, 1>;
+type ToUartChannelPublisher =
+    Publisher<'static, ThreadModeRawMutex, ToUartBuf, 4, 1, 1>;
+type ToUartChannelSubscriber =
+    Subscriber<'static, ThreadModeRawMutex, ToUartBuf, 4, 1, 1>;
+static TO_UART: ToUartChannel = PubSubChannel::new();
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let board = Board::new();
 
     let uart_tx = board.uart_tx;
-    let usb_cdc_rx = board.usb_cdc_rx;
-    spawner.must_spawn(usb_to_uart(uart_tx, usb_cdc_rx));
+    let to_uart_sub = TO_UART.subscriber().unwrap();
+    spawner.must_spawn(uart_sender(uart_tx, to_uart_sub));
 
     let usb_cdc_tx = board.usb_cdc_tx;
     let to_pc_sub = TO_USB.subscriber().unwrap();
@@ -46,6 +54,10 @@ async fn main(spawner: Spawner) {
     let to_pc_pub = TO_USB.publisher().unwrap();
     spawner.must_spawn(uart_receiver(uart_rx, to_pc_pub));
 
+    let usb_cdc_rx = board.usb_cdc_rx;
+    let to_uart_pub = TO_UART.publisher().unwrap();
+    spawner.must_spawn(usb_receiver(usb_cdc_rx, to_uart_pub));
+
     let mut usb = board.usb;
     loop {
         usb.run().await;
@@ -53,25 +65,23 @@ async fn main(spawner: Spawner) {
 }
 
 #[embassy_executor::task]
-async fn usb_to_uart(
+async fn uart_sender(
     mut uart_tx: UartTx<'static, Async>,
-    mut cdc_rx: cdc_acm::Receiver<
-        'static,
-        usb::Driver<'static, peripherals::USB>,
-    >,
+    mut to_uart_sub: ToUartChannelSubscriber,
 ) {
     loop {
-        cdc_rx.wait_connection().await;
-        let mut buf = [0; 64];
-        loop {
-            if let Ok(n) = cdc_rx.read_packet(&mut buf).await {
-                let data = &buf[..n];
-                if let Err(e) = uart_tx.write_all(data).await {
-                    #[cfg(feature = "defmt")]
-                    error!("UART TX err: {:?}", e);
-                }
-            } else {
-                break;
+        let buf = match to_uart_sub.next_message().await {
+            WaitResult::Lagged(n) => {
+                #[cfg(feature = "defmt")]
+                error!("Missed {:?} bytes to send to payload", n);
+                None
+            }
+            WaitResult::Message(buf) => Some(buf),
+        };
+        if let Some(buf) = buf {
+            if let Err(e) = uart_tx.write_all(&buf).await {
+                #[cfg(feature = "defmt")]
+                error!("UART TX err: {:?}", e);
             }
         }
     }
@@ -122,6 +132,28 @@ async fn usb_sender(
                     error!("CDC TX err: {:?}", e);
                     break;
                 }
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn usb_receiver(
+    mut cdc_rx: cdc_acm::Receiver<
+        'static,
+        usb::Driver<'static, peripherals::USB>,
+    >,
+    to_uart_pub: ToUartChannelPublisher,
+) {
+    loop {
+        cdc_rx.wait_connection().await;
+        let mut buf = [0; 64];
+        loop {
+            if let Ok(n) = cdc_rx.read_packet(&mut buf).await {
+                let data: ToUartBuf = buf[..n].try_into().unwrap();
+                to_uart_pub.publish(data).await;
+            } else {
+                break;
             }
         }
     }
